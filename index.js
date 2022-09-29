c4a3f448a95cfaf6eb2b88c1c8c573d7eb7edb7c
Rico <grico484@gmail.com>
2022-09-28 21:06:25 -0400

    Added buttons without side menu

--- index.js
+++ index.js
@@ -3,11 +3,9 @@ const buttonMenu = document.querySelector('.menu-img');
       buttonMenu.addEventListener('click', ()=> {
         document.querySelector('.mobile-nav').classList.add('show');
         document.querySelector('.overlay').style.display = 'block';
-        document.body.classList.add('lock');
       });
 
       document.querySelector('.close-menu').addEventListener('click', ()=> {
         document.querySelector('.mobile-nav').classList.remove('show');
         document.querySelector('.overlay').style.display = 'none';
-        document.body.classList.remove('lock');
       });
